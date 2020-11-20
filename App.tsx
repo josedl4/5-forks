f7a5ba53fce9d6c6e98a92ec7972aca2fdfec320
Jose Luis Martin <joseop21@gmail.com>
2020-11-20 23:20:11 +0100

    Added tab icons

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Navigation from './app/navigations/Navigation';
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation></Navigation>
   );
 }
 
--- app/navigations/Navigation.tsx
+++ app/navigations/Navigation.tsx
@@ -1,6 +1,6 @@
 // Imports
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, Route, RouteProp } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // Screens
@@ -9,18 +9,57 @@ import FavouritesStack from './FavouritesStack';
 import RestaurantsStack from './RestaurantsStack';
 import SearchStack from './SearchStack';
 import TopRestaurantsStack from './TopRestaurantsStack';
+import { Icon } from 'react-native-elements';
 
 
 const Tab = createBottomTabNavigator();
 
+const ACTIVE_COLOR = '#00a680';
+const INACTIVE_COLOR = '#646464';
+
+const SECTIONS: { [any: string]: { id: string, iconName: string } } = {
+    restaurants: {
+        id: 'restaurants',
+        iconName: 'compass-outline'
+    },
+    top: {
+        id: 'top',
+        iconName: 'star-outline'
+    },
+    favs: {
+        id: 'favs',
+        iconName: 'heart-outline'
+    },
+    search: {
+        id: 'search',
+        iconName: 'magnify'
+    },
+    account: {
+        id: 'account',
+        iconName: 'home-outline'
+    }
+};
+
+const screenOptions = (route: Route<string, object | undefined>, color: string) => (
+    <Icon type="material-community" name={SECTIONS[route.name] ? SECTIONS[route.name].iconName : ''} size={22} color={color}></Icon>
+)
+
 const Navigation = () => (
     <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name="restaurants" component={RestaurantsStack} options={{ title: 'Restaurants' }} />
-            <Tab.Screen name="top" component={TopRestaurantsStack} options={{ title: 'Top 5' }} />
-            <Tab.Screen name="favs" component={FavouritesStack} options={{ title: 'Favourites' }} />
-            <Tab.Screen name="search" component={SearchStack} options={{ title: 'Search' }} />
-            <Tab.Screen name="account" component={AccountStack} options={{ title: 'Account' }} />
+        <Tab.Navigator
+            initialRouteName="restaurants"
+            tabBarOptions={{
+                inactiveTintColor: ACTIVE_COLOR,
+                activeTintColor: INACTIVE_COLOR
+            }}
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color }) => screenOptions(route, color)
+            })}>
+            <Tab.Screen name={SECTIONS.restaurants.id} component={RestaurantsStack} options={{ title: 'Restaurants' }} />
+            <Tab.Screen name={SECTIONS.top.id} component={TopRestaurantsStack} options={{ title: 'Top 5' }} />
+            <Tab.Screen name={SECTIONS.favs.id} component={FavouritesStack} options={{ title: 'Favourites' }} />
+            <Tab.Screen name={SECTIONS.search.id} component={SearchStack} options={{ title: 'Search' }} />
+            <Tab.Screen name={SECTIONS.account.id} component={AccountStack} options={{ title: 'Account' }} />
         </Tab.Navigator>
     </NavigationContainer >
 );
